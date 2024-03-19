83955455b6f3d4b32c7fb59f5dc1e1a7dbf870c5
SeongHun <tjdgns5506@gmail.com>
2024-03-19 17:55:42 +0900

    test: 다시 배포

--- javascript/service.js
+++ javascript/service.js
@@ -4,6 +4,13 @@ async function submitForm(event) {
 
     //시작 알리기
     window.alert('시작합니다')
+    // 페이지 이동
+    var url = "result.html" +
+        "?busStopName=" + encodeURIComponent(busStopName) +
+        "&busNumber=" + encodeURIComponent(busNumber) +
+        "&targetNumber=" + encodeURIComponent(targetNumber);
+
+    window.location.href = url;
 
 
 
@@ -23,7 +30,7 @@ async function submitForm(event) {
 
     fetch(springURL);
 
-    var resultMessage =`${busStopNumber} 정류장에 ${busNumber} 버스가 ${targetNumber} 전 역일때.`;
+  
 
     //window.location.href = "result.html";
     // 변수 확인 로그
@@ -31,30 +38,9 @@ async function submitForm(event) {
     console.log("버스 번호: " + busNumber);
     console.log("몇 정류장 전에 알려드릴까요?: " + targetNumber);
 
-    // 여기서 데이터를 사용하거나 서버로 전송하는 등의 작업을 수행할 수 있습니다.
-    // 예: ajax 요청, 페이지 이동 등
 
-    //봉인
-    // 서비스워커 컨트롤러 확인 후 메시지 전송, 서비스워커에 데이터 전달
-    // if (navigator.serviceWorker.controller) {
-    //     navigator.serviceWorker.controller.postMessage({
-    //         type: 'personal',
-    //         data: {
-    //             busStopNumber: busStopNumber,
-    //             busNumber: busNumber,
-    //             targetNumber: targetNumber
-    //         }
-    //     });
-    // } else {
-    //     console.error('Service Worker controller is null.');
-    // }
-    // 페이지 이동
-    var url = "result.html" +
-        "?busStopName=" + encodeURIComponent(busStopName) +
-        "&busNumber=" + encodeURIComponent(busNumber) +
-        "&targetNumber=" + encodeURIComponent(targetNumber);
-
-    window.location.href = url;
+    
+    
 
 }
 
